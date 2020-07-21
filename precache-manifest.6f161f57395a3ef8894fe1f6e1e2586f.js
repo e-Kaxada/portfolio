d32b77d84d9981d45e03395d598bd143bfa91daf
e-Kaxada <enocksdha@gmail.com>
2020-07-21 21:24:32 +0300

    Updates

---
+++ precache-manifest.6f161f57395a3ef8894fe1f6e1e2586f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485e9e2d45285c1198e631a609fa90b0",
+    "url": "/e-Kaxada.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "6609c9a60ee7c571c26b",
+    "url": "/e-Kaxada.github.io/portfolio/static/css/main.562a95c7.chunk.css"
+  },
+  {
+    "revision": "514082dcb79fecfa9d4e",
+    "url": "/e-Kaxada.github.io/portfolio/static/js/2.d9e39423.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/e-Kaxada.github.io/portfolio/static/js/2.d9e39423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6609c9a60ee7c571c26b",
+    "url": "/e-Kaxada.github.io/portfolio/static/js/main.bdf14c88.chunk.js"
+  },
+  {
+    "revision": "498bdfad06e24e36a85e",
+    "url": "/e-Kaxada.github.io/portfolio/static/js/runtime-main.37c0f9c0.js"
+  }
+]);
