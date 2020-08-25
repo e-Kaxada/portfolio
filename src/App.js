7bfaeac1271183117b0d5866b003401630cb26bf
e-Kaxada <enocksdha@gmail.com>
2020-08-25 13:20:01 +0300

    portfolio redesign

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from "react";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Contacts from "./components/Contacts/Contacts";
-import Projects from "./components/Projects/Projects";
-import NavBar from "./components/NavBar/NavBar";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import "./App.css";
+import './App.css'
+import Main from './main/Main'
 
 function App() {
-  AOS.init();
   return (
     <div className="App" id="App">
-      <Home />
-      <NavBar />
-      <div className="combined">
-        <About />
-        <Projects />
-      </div>
-      <Contacts />
+      <Main/>
     </div>
   );
 }
