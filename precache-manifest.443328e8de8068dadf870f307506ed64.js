82b8b9f7fa1d7ee8b27c5d2532d9b76f8c4c7dae
e-Kaxada <enocksdha@gmail.com>
2020-08-25 13:35:44 +0300

    Updates

---
+++ precache-manifest.443328e8de8068dadf870f307506ed64.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b97e204f9341b77a300a5cf3a8597fe",
+    "url": "/e-Kaxada.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "bf21f69b2071b375d09e",
+    "url": "/e-Kaxada.github.io/portfolio/static/css/main.41559438.chunk.css"
+  },
+  {
+    "revision": "b863d67df7c6d958dede",
+    "url": "/e-Kaxada.github.io/portfolio/static/js/2.d9faadde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/e-Kaxada.github.io/portfolio/static/js/2.d9faadde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf21f69b2071b375d09e",
+    "url": "/e-Kaxada.github.io/portfolio/static/js/main.ac9a76ea.chunk.js"
+  },
+  {
+    "revision": "498bdfad06e24e36a85e",
+    "url": "/e-Kaxada.github.io/portfolio/static/js/runtime-main.37c0f9c0.js"
+  },
+  {
+    "revision": "da5e1b06d905f9606bbcca3bfce5186a",
+    "url": "/e-Kaxada.github.io/portfolio/static/media/blog.da5e1b06.jpg"
+  },
+  {
+    "revision": "e7e67cb16ca8787ddb5ef1f6295b99c6",
+    "url": "/e-Kaxada.github.io/portfolio/static/media/ecommerce.e7e67cb1.jpg"
+  },
+  {
+    "revision": "b785b1d435fff68db45ba460b4d93e85",
+    "url": "/e-Kaxada.github.io/portfolio/static/media/enock.b785b1d4.jpg"
+  },
+  {
+    "revision": "39c2d77e421fdca6ac1b3a3120527429",
+    "url": "/e-Kaxada.github.io/portfolio/static/media/restaurant.39c2d77e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/e-Kaxada.github.io/portfolio/precache-manifest.8026ef0167290e60a364be3593da7d1a.js"
+  "/e-Kaxada.github.io/portfolio/precache-manifest.443328e8de8068dadf870f307506ed64.js"
 );
 
 self.addEventListener('message', (event) => {
